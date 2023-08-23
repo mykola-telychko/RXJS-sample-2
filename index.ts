58f0b6dc6e033db094d356839caa3cbd5e36d1c2
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-23 22:04:52 +0300

    fin

--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@ import { sample } from 'rxjs/operators';
 
 // https://www.learnrxjs.io/learn-rxjs/operators/filtering/sample
 // Example 2: Sample source when interval emits
-const source = zip(
+const srcZipInterval = zip(
   //emit 'Joe', 'Frank' and 'Bob' in sequence
   from(['Joe', 'Frank', 'Bob']), //emit value every 2s
   interval(2000)
 );
+
 //sample last emitted value from source every 2.5s
-const example = source.pipe(sample(interval(2500)));
-const subscribe = example.subscribe(val => console.log(val));
-//output: ["Joe", 0]...["Frank", 1]...........
+// union zip-1-element and zip-2-element
+const example = srcZipInterval.pipe(sample(interval(2500)));
+const subscribe = example.subscribe((val) => console.log(val));
+//output: ["Joe", 0]...["Frank", 1]
